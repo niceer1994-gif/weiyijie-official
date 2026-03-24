e21e63dd0a62966046f9e21948949682290ec096
Weiyijie Dev <dev@weiyijiebio.com>
2026-03-24 17:57:28 +0800

    Fix: add base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // 适配GitHub Pages子路径，绑定自定义域名后可以删除这行
+  base: '/weiyijie-official',
   vite: {
     plugins: [tailwindcss()]
   }
